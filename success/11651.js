b7d759a9a286d9fdf75943a8a601f5cc8ff24b0c
ydk1204 <ejrbdi@naver.com>
2022-11-15 14:00:48 +0900

    10989, 11651

---
+++ success/11651.js
@@ -0,0 +1,20 @@
+const BAEAKJOONFILE = "/dev/stdin";
+const VSCODEFILE = "./coding/example.txt";
+
+const fs = require("fs");
+let input = fs.readFileSync(VSCODEFILE).toString().trim().split("\n");
+
+const N = input.shift();
+const arr = input.map((x) => x.split(" ").map(Number));
+
+arr.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  return a[1] - b[1];
+});
+
+let answer = "";
+arr.forEach((v) => {
+  answer += v[0] + " " + v[1] + "\n";
+});
+
+console.log(answer);

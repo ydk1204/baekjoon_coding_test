e1f90965b03f1980e8ef192dddb3e0f16ae5a307
ydk1204 <ejrbdi@naver.com>
2022-12-09 20:00:27 +0900

    2798 주석

--- success/2798.js
+++ success/2798.js
@@ -10,6 +10,7 @@ const [N, M] = input[0].map(x => parseInt(x));
 const [...card] = input[1].map(x => parseInt(x));
 let answer = 0;
 
+// 3중 반복문을 사용해서 각 카드의 숫자를 더한 후 M과 가장 차이가 적은 수를 출력
 for (let i = 0; i <= card.length - 3; i++) {
   for (let j = i + 1; j <= card.length - 2; j++) { 
     for (let a = j + 1; a < card.length; a++) { 

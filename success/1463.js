351fd92ebc81b9e748270eda8b96ed45056714b9
ydk1204 <ejrbdi@naver.com>
2022-12-28 16:59:30 +0900

    1463

---
+++ success/1463.js
@@ -0,0 +1,16 @@
+const BAEAKJOONFILE = "/dev/stdin";
+const VSCODEFILE = "./coding/example.txt";
+
+const fs = require("fs");
+const input = fs.readFileSync(VSCODEFILE).toString();
+const x = Number(input);
+const DP = new Array(x + 1).fill(0);
+
+for (let i = 2; i <= x; i++) {
+  DP[i] = DP[i - 1] + 1;
+
+  if (i % 2 === 0) DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+  if (i % 3 === 0) DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+}
+
+console.log(DP[x]);

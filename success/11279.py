c1b5236cb0b20398753a7a96ee1fd7d6fe32ba98
ydk1204 <ejrbdi@naver.com>
2023-11-14 19:38:41 +0900

    11279

---
+++ success/11279.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+
+# vscode 테스트
+sys.stdin = open('coding/example.txt', 'r')  # 테스트 케이스 불러오기
+
+# 백준 테스트
+# input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -x)
